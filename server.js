8ab15d129816b58dff05306c172b0664326f9dd8
Ben <WBHankins93@Bens-MacBook-Air.local>
2018-10-19 15:48:46 -0500

    removed node-modules

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app     = express();
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('OpenRoad is alive')
+});
+
+
+
+
+app.listen(3000, (req, res) => {
+  console.log('Ayyyyy Im workin here')
+});
