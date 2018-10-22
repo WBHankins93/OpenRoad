dae4e970a88505fc95b39decb104a8b383a98d44
Ben <WBHankins93@Bens-MacBook-Air.local>
2018-10-22 15:15:00 -0500

    added to index and created route for new.ejs

--- controllers/trip.js
+++ controllers/trip.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Trip = require('../models/trip');
 
+// Route that gives us the main index
 router.get('/', (req, res)=>{
   Trip.find({}, (err, foundTrip)=>{
     res.render('trips/index.ejs', {
@@ -9,6 +10,16 @@ router.get('/', (req, res)=>{
     });
   })
 });
+
+/// Route for Add Trip
+router.get('/new', (req, res) => {
+  Trip.find({}, (err, allTrips) => {
+    res.render('trips/new.ejs', {
+      trips: allTrips
+    });
+  })
+
+})
 
 
 
