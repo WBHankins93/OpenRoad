883e9f5366dd385aa7a90fdd7c4521c043b88493
Ben <WBHankins93@Bens-MacBook-Air.local>
2018-10-23 14:08:27 -0500

    login page, login router connected to server

--- server.js
+++ server.js
@@ -8,15 +8,18 @@ require('./db/db');
 
 // Require our controller
 const tripController = require('./controllers/trip');
+const authController = require('./controllers/auth');
 
 // Apply middleware here
-app.use(express.static( 'public' ) );
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
 
 
 // Call the controller here
-app.use('/trip', tripController)
+app.use('/trip', tripController);
+app.use('/auth', authController);
+
 
 
 app.get('/', (req, res) => {
